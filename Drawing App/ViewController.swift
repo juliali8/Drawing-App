ccab315fe7d0a71062a50d8291c07241ebad19e9
Juila Li <lijulia@wustl.edu>
2018-07-09 12:36:32 -0500

    Initial Commit

---
+++ Drawing App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drawing App
+//
+//  Created by Julia Li on 7/9/18.
+//  Copyright © 2018 Julia Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
